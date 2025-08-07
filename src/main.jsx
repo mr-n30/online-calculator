56ad51e5642c3feaef12ad75ae1cdb52bddb7c36
mr-n30 <mr-n30@localhost>
2025-08-06 19:41:55 -0500

    updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { useState } from 'react'
 
-const NumberContainer = () => {
+
+const NumberContainer = ({ setInput }) => {
   const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '.', '=', '+', '-', '*', '/']
+
+  const handleClick = (num) => {
+    setInput((prev) => {
+      // TODO: make this a switch statement
+      if (prev === 'Enter a number') {
+        return String(num)
+      } else if (prev === '0' || prev === '=') { // TODO: add remaining operators
+        return String(num)
+      }
+
+      return prev + String(num)
+    })
+  }
+
   const jsx = numbers.map((num) => (
-    <button key={num} className="number-button bg-gray-100" onClick={() => console.log(num)}>
+    <button key={num} className="number-button bg-gray-100" onClick={() => handleClick(num)}>
       {num}
     </button>
   ))
+
   return (
     <div className="number-button-container">
       {jsx}
@@ -16,29 +33,36 @@ const NumberContainer = () => {
   )
 }
 
-const ActionButtonRow = () => {
+const ActionButtonRow = ({ setInput }) => {
+  const handleClear = () => {
+    setInput('0')
+  }
+
   return (
     <div className="action-button-row">
-      <button className="clear-button">CLEAR</button>
+      <button className="clear-button" onClick={handleClear}>CLEAR</button>
       <button className="delete-button">DELETE</button>
     </div>
   )
 }
 
-const Screen = () => {
+const Screen = ({ input }) => {
+  console.log(input)
   return (
     <div className="screen">
-      Enter a number
+      {input}
     </div>
   )
 }
 
 const Calculator = () => {
+  const [input, setInput] = useState('Enter a number')
+
   return (
     <div className="calculator">
-      <Screen />
-      <ActionButtonRow />
-      <NumberContainer />
+      <Screen input={input} />
+      <ActionButtonRow setInput={setInput} />
+      <NumberContainer setInput={setInput} />
     </div>
   )
 }
